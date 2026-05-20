9f681690969c8e4cce867fbf62798630a34d8bd9
Pranav-Sagar <dyapapranav@gmail.com>
2026-05-20 16:16:46 +0530

    Updated Navabr and Footer

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="nav-wrapper">
       <div className="nav-container">
@@ -11,7 +14,7 @@ const Navbar = () => {
           </div>
           <p className="tagline">Build | Scale | Lead</p>
         </div>
-        <ul className="nav-links">
+        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
           <li className="active">Home</li>
           <li>About</li>
           <li>
@@ -24,6 +27,14 @@ const Navbar = () => {
           <button className="call-btn">
             Book a Call <span className="arrow">→</span>
           </button>
+        </div>
+        <div
+          className={`hamburger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
 
       </div>
